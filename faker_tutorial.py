6b99079dc9b19ceecb408e46a0295488eb1f4363
Kim SungWoo <gokorea1214@kakao.com>
2023-08-23 01:36:51 +0900

    faker 기초개념 강의 필기, items함수 기본

--- faker_tutorial.py
+++ faker_tutorial.py
@@ -1,16 +1,33 @@
 from faker import Faker
 
-faker = Faker("ko_KR")
+faker = Faker()
 
 print(faker.name())
 print(faker.first_name())
-print(faker.last_name())
-print(faker.word())
+# print(faker.last_name())
+# print(faker.word())
 print(faker.sentence())
 print(faker.text())
 
 """
-비밀번호 생성 -> word
-게시글의 제목 -> sentence
-게시글의 내용 -> text
+비밀번호 생성 -> faker.word()
+게시글의 제목 -> faker.sentence()
+게시글의 내용 -> faker.text()
 """
+# first_name = faker.first_name()
+# last_name = faker.last_name()
+# email = f"{last_name}@{faker.domain_name()}"
+# print(first_name)
+# print(last_name)
+# print(email)
+
+# class ArticleReadTest(APITestCase):
+#     @classmethod
+#     def setUpTestData(cls):
+#         cls.faker = Faker()
+#         cls.articles = []
+#         cls.rnd_email = f"{cls.faker.last_name()}@{cls.faker.domain_name()}"
+#         for i in range(10):
+#             cls.user = User.objects.create_user(cls.rnd_email, cls.faker.first_name(), cls.faker.last_name(), cls.faker.word())
+#             print(cls.user)
+#             # ('test@kakao.com','tester', 'testuser','popk1214')
---
+++ items_func.py
@@ -0,0 +1,7 @@
+
+# items.py
+
+my_dict = {"키값1":"밸류값", "키값2":"some value", "키값3":"kkh"}
+
+for key, value in my_dict.items():
+		print(key)
